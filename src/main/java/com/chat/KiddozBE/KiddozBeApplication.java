ef54c19a107941f6c0db0f20301ee59221d501fd
913-Iulia-Ioana-Farcas <72136515+iuliaafarcas@users.noreply.github.com>
2023-01-08 15:36:45 +0200

    created chat

---
+++ src/main/java/com/chat/KiddozBE/KiddozBeApplication.java
@@ -0,0 +1,13 @@
+package com.chat.KiddozBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiddozBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiddozBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chat/KiddozBE/KiddozBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.KiddozBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiddozBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
